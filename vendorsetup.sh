01c8b51db77267689f969ef636f1168e2e3b38ae
Quartz750 <145698054+Quartz750@users.noreply.github.com>
2026-05-13 00:18:04 +0800

    Comment out lunch combo options in vendorsetup.sh
    Comment out lunch combo options for portov.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_portov-user
-add_lunch_combo omni_portov-userdebug
-add_lunch_combo omni_portov-eng
+# add_lunch_combo omni_portov-user
+# add_lunch_combo omni_portov-userdebug
+# add_lunch_combo omni_portov-eng
