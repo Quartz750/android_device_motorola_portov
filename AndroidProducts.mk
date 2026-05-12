9e4b9f899aae97fc60af36de84a534a91bf68188
Quartz750 <145698054+Quartz750@users.noreply.github.com>
2026-05-13 07:41:51 +0800

    Fix LOCAL_PATH and update PRODUCT_MAKEFILES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,11 +5,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := $(call my-dir)
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/omni_portov.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_portov-user \
-    omni_portov-userdebug \
-    omni_portov-eng
+    $(LOCAL_DIR)/omni_portov.mk
