f9b29212b9d2e204f820d05acd7410ae427b53de
Quartz750 <145698054+Quartz750@users.noreply.github.com>
2026-05-13 00:50:23 +0800

    Fix path for omni_portov.mk in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := $(call my-dir)
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_portov.mk
+    $(LOCAL_PATH)/omni_portov.mk
 
 COMMON_LUNCH_CHOICES := \
     omni_portov-user \
